9a7726428cecbdca31b8d956c4778a7ec4a59c58
FadeZhanger <dzhan96@uwo.ca>
2017-07-22 00:29:44 -0400

    goddammit

---
+++ Finger_Tapping.js
@@ -0,0 +1,51 @@
+
+var counter = 0;
+var timeRemaining = 10;
+var inGame = false;
+var timer;
+
+
+function updateText(target, text) {
+    document.getElementById(target).innerHTML = text.toString();
+}
+
+function addToCounter() {
+    if (!inGame) {
+        inGame = true;
+        timer = setInterval(updateTimer, 1000);
+    }
+    counter++;
+    updateText("display", counter);
+}
+
+function updateTimer() {
+    if (inGame) {
+        --timeRemaining;
+    }
+
+    if (timeRemaining === -1) {
+        inGame = false;
+        clearInterval(timer);
+        alert("You tapped: " + counter + " times!");
+        timeRemaining = 10;
+        reset();
+    }
+    updateText('timer', timeRemaining + " seconds");
+}
+
+function reset() {
+    counter = 0;
+    updateText("display", counter);
+
+    if (inGame) {
+        clearInterval(timer);
+        inGame = false;
+        timeRemaining = 10;
+        updateText('timer', timeRemaining + " seconds");
+    }
+}
+
+
+
+
+
