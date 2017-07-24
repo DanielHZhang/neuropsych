759a3998630bf2e0c0a9d830a264b40ce36108eb
FadeZhanger <dzhan96@uwo.ca>
2017-07-23 21:27:48 -0400

    test if fits on mobile

--- Grooved_Pegboard.js
+++ Grooved_Pegboard.js
@@ -7,6 +7,14 @@ var startTime;
 
 $.mobile.loading().hide();
 
+
+window.onload = function() {
+    $.vmouse.moveDistanceThreshold = "100px";
+    console.log("ran");
+};
+
+
+
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
  * Using Math.round() will give you a non-uniform distribution!
@@ -125,6 +133,8 @@ $(function () {
         if (event.which === 1) {
             event.preventDefault();
         }
+        event.stopPropagation();
+
         _ref = this.getBoundingClientRect();
         top = _ref.top;
         left = _ref.left;
@@ -163,127 +173,86 @@ $(function () {
 });
 
 
-
-
-
-// (function () {
+// window.onload = function () {
+//     $(function () {
+//         var dragging = false, target_wp, o_x, o_y, h_x, h_y, last_angle;
+//         $('.handle').mousedown(function (e) {
+//             h_x = e.pageX;
+//             h_y = e.pageY; // clicked point
+//             e.preventDefault();
+//             e.stopPropagation();
+//             dragging = true;
+//             target_wp = $(e.target).closest('.draggable_wp');
+//             if (!target_wp.data("origin")) target_wp.data("origin", {
+//                 left: target_wp.offset().left,
+//                 top: target_wp.offset().top
+//             });
+//             o_x = target_wp.data("origin").left;
+//             o_y = target_wp.data("origin").top; // origin point
 //
-//     init();
+//             last_angle = target_wp.data("last_angle") || 0;
+//         });
 //
-// }).call(this);
-
-
-// var mc = new Hammer(target);
-// var pinch = new Hammer.Pinch();
-// var rotate = new Hammer.Rotate();
-// pinch.recognizeWith(rotate);
-// mc.add([pinch, rotate]);
+//         $(document).mousemove(function (e) {
+//             if (dragging) {
+//                 var s_x = e.pageX,
+//                     s_y = e.pageY; // start rotate point
+//                 if (s_x !== o_x && s_y !== o_y) { //start rotate
+//                     var s_rad = Math.atan2(s_y - o_y, s_x - o_x); // current to origin
+//                     s_rad -= Math.atan2(h_y - o_y, h_x - o_x); // handle to origin
+//                     s_rad += last_angle; // relative to the last one
+//                     var degree = (s_rad * (360 / (2 * Math.PI)));
+//                     target_wp.css('-moz-transform', 'rotate(' + degree + 'deg)');
+//                     target_wp.css('-moz-transform-origin', '50% 50%');
+//                     target_wp.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+//                     target_wp.css('-webkit-transform-origin', '50% 50%');
+//                     target_wp.css('-o-transform', 'rotate(' + degree + 'deg)');
+//                     target_wp.css('-o-transform-origin', '50% 50%');
+//                     target_wp.css('-ms-transform', 'rotate(' + degree + 'deg)');
+//                     target_wp.css('-ms-transform-origin', '50% 50%');
+//                 }
+//             }
+//         }); // end mousemove
 //
+//         $(document).mouseup(function (e) {
+//             if (dragging) {
+//                 dragging = false;
+//                 var s_x = e.pageX,
+//                     s_y = e.pageY;
 //
-// mc.on("pinch rotate", function (ev) {
-//     console.log(typeof(ev.));
-//     target.style.webkitTransform = "rotate(" + 90 + "deg)";
-//
-// });
-
-
-// target.addEventListener("touchmove", function (event) {
-//         var rotation = event.rotation;
-//
-//         // This isn't a fun browser!
-//         if (!rotation) {
-//             rotation = Math.arctan2(event.touches[0].pageY - event.touches[1].pageY,
-//                     event.touches[0].pageX - event.touches[1].pageX) * 180 / Math.PI;
-//         }
-//
-//         // Take into account vendor prefixes, which I haven't done.
-//         this.style.transform = "rotate(" + rotation + "deg)";
+//                 // Saves the last angle for future iterations
+//                 var s_rad = Math.atan2(s_y - o_y, s_x - o_x); // current to origin
+//                 s_rad -= Math.atan2(h_y - o_y, h_x - o_x); // handle to origin
+//                 s_rad += last_angle;
+//                 target_wp.data("last_angle", s_rad);
+//             }
+//         })
 //     });
-
-
-/*
- window.onload = function() {
- $(function () {
- var dragging = false,
- target_wp,
- o_x, o_y, h_x, h_y, last_angle;
- $('.handle').mousedown(function (e) {
- h_x = e.pageX;
- h_y = e.pageY; // clicked point
- e.preventDefault();
- e.stopPropagation();
- dragging = true;
- target_wp = $(e.target).closest('.draggable_wp');
- if (!target_wp.data("origin")) target_wp.data("origin", {
- left: target_wp.offset().left,
- top: target_wp.offset().top
- });
- o_x = target_wp.data("origin").left;
- o_y = target_wp.data("origin").top; // origin point
-
- last_angle = target_wp.data("last_angle") || 0;
- })
-
- $(document).mousemove(function (e) {
- if (dragging) {
- var s_x = e.pageX,
- s_y = e.pageY; // start rotate point
- if (s_x !== o_x && s_y !== o_y) { //start rotate
- var s_rad = Math.atan2(s_y - o_y, s_x - o_x); // current to origin
- s_rad -= Math.atan2(h_y - o_y, h_x - o_x); // handle to origin
- s_rad += last_angle; // relative to the last one
- var degree = (s_rad * (360 / (2 * Math.PI)));
- target_wp.css('-moz-transform', 'rotate(' + degree + 'deg)');
- target_wp.css('-moz-transform-origin', '50% 50%');
- target_wp.css('-webkit-transform', 'rotate(' + degree + 'deg)');
- target_wp.css('-webkit-transform-origin', '50% 50%');
- target_wp.css('-o-transform', 'rotate(' + degree + 'deg)');
- target_wp.css('-o-transform-origin', '50% 50%');
- target_wp.css('-ms-transform', 'rotate(' + degree + 'deg)');
- target_wp.css('-ms-transform-origin', '50% 50%');
- }
- }
- }) // end mousemove
-
- $(document).mouseup(function (e) {
- if ( dragging ) {
- dragging = false
- var s_x = e.pageX,
- s_y = e.pageY;
-
- // Saves the last angle for future iterations
- var s_rad = Math.atan2(s_y - o_y, s_x - o_x); // current to origin
- s_rad -= Math.atan2(h_y - o_y, h_x - o_x); // handle to origin
- s_rad += last_angle;
- target_wp.data("last_angle", s_rad);
- }
- })
- })
- $(function() {
- var startX, startY;
- $( ".draggable_wp" ).draggable({
- start:function(event, ui) {
- var offset = $(".draggable_wp").offset();
- startX = offset.left;
- startY = offset.top;
- },
- stop:function(event, ui) {
- var origin = $(".draggable_wp").data("origin");
- var offset = $(".draggable_wp").offset();
- if ( origin ) {
- $(".draggable_wp").data("origin", {
- left:origin.left + offset.left - startX,
- top:origin.top + offset.top - startY
- });
- }
- else {
- $(".draggable_wp").data("origin", {
- left:offset.left,
- top:offset.top
- });
- }
- }
- });
- });
-
- };*/
+//     $(function () {
+//         var startX, startY;
+//         $(".draggable_wp").draggable({
+//             start: function (event, ui) {
+//                 var offset = $(".draggable_wp").offset();
+//                 startX = offset.left;
+//                 startY = offset.top;
+//             },
+//             stop: function (event, ui) {
+//                 var origin = $(".draggable_wp").data("origin");
+//                 var offset = $(".draggable_wp").offset();
+//                 if (origin) {
+//                     $(".draggable_wp").data("origin", {
+//                         left: origin.left + offset.left - startX,
+//                         top: origin.top + offset.top - startY
+//                     });
+//                 }
+//                 else {
+//                     $(".draggable_wp").data("origin", {
+//                         left: offset.left,
+//                         top: offset.top
+//                     });
+//                 }
+//             }
+//         });
+//     });
+//
+// };
